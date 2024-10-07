36b6d651a542a6c6692191df725acf5c1a4697c9
Tamimikbal <112070850+Tamimikbal@users.noreply.github.com>
2024-10-07 13:08:04 +0600

    handlers.js

--- functions/handlers.js
+++ functions/handlers.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
-const botName = "🌇 MULTI MUSIX ";
-const rtxText = "GlaceYT";
+const botName = "🌇 Atomic Music ";
+const rtxText = "Unknown";
 const version = "Latest@ v1.7"; 
 const startTime = Date.now();
 
